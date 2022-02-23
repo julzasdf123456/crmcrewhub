f131261380edbb3150fe2515c5bad2586319e56e
Julio Lopez <julzasdf123456@gmail.com>
2022-02-23 20:44:31 +0800

    February 23, 2022

--- app/src/main/java/com/lopez/julz/crmcrewhub/api/BaseURL.java
+++ app/src/main/java/com/lopez/julz/crmcrewhub/api/BaseURL.java
@@ -3,6 +3,6 @@ package com.lopez.julz.crmcrewhub.api;
 public class BaseURL {
     public static String baseUrl() {
 //        return "http://192.168.10.161/crm-noneco/public/api/";
-        return "http://192.168.110.95/crm-noneco/public/api/";
+        return "http://192.168.103.79/crm-noneco/public/api/";
     }
 }
